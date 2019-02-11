1a73006937a51635fa2dfd14b5b53f4b37553d59
Anton Weiss <anton@otomato.link>
2019-02-11 17:27:49 +0200

    Python plugin example

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine
+FROM python:3.6-alpine
 LABEL MAINTAINER="{{ .Maintainer }}"  CATEGORY="Codefresh Plugins"
 WORKDIR /home
 ENV CF_PLUGIN_NAME="{{ .Name }}"
---
+++ entrypoint.py
@@ -0,0 +1,23 @@
+#!/bin/python
+import time
+import os
+#### Prepare functions for prettier output
+def msg(message):
+    print ("\033[92m INFO [{}] ---> {}\033[0m".format(time.strftime("%H:%M:%S"), message))
+def warn(message):
+    print ("\033[93m WARN [{}] ---> {}\033[0m".format(time.strftime("%H:%M:%S"), message))
+def err(message):
+    print ("\033[91m ERR  [{}] ---> {}\033[0m".format(time.strftime("%H:%M:%S"), message))
+
+msg("Running codefresh plugin {}".format(os.getenv('CF_PLUGIN_NAME', "undefined")))
+# #### Check for required environment variables
+if os.getenv("PLUGIN_RESULT") == None:
+    err("Need to set PLUGIN_RESULT")
+
+msg ("Environment variable PLUGIN_RESULT is: {}".format(os.getenv("PLUGIN_RESULT")))
+msg ("Codefresh account is {}".format(os.getenv("CF_ACCOUNT", "undefined")))
+
+if os.getenv("PLUGIN_RESULT") == "SUCCESS":
+    msg ("Requested PLUGIN_RESULT is SUCCESS. Have a great day!")
+else:
+    err ("Requested PLUGIN_RESULT is {}. Failing.".format(os.getenv("PLUGIN_RESULT")))
